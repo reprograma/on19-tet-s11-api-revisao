03539362a5b2d21b614a6a32509d332d1e9f1415
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-10-27 17:12:16 -0300

    create clientes controller

---
+++ exercicios/para-casa/MVC/src/controller/clientes.js
@@ -0,0 +1,15 @@
+const create = (req, res) => {
+  return res.send("ola mundo")
+}
+const show = (req, res) => {}
+const index = (req, res) => {}
+const update = (req, res) => {}
+const destroy = (req, res) => {}
+
+module.exports = {
+  create,
+  show,
+  index,
+  update,
+  destroy
+}
