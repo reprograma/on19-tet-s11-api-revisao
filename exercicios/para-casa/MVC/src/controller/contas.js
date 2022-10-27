6aaaec7c628dc72ebd9f69807a599d58e659a163
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-10-27 17:13:54 -0300

    create contas controller file

---
+++ exercicios/para-casa/MVC/src/controller/contas.js
@@ -0,0 +1,13 @@
+const create = (req, res) => {}
+const show = (req, res) => {}
+const index = (req, res) => {}
+const update = (req, res) => {}
+const destroy = (req, res) => {}
+
+module.exports = {
+  create,
+  show,
+  index,
+  update,
+  destroy
+}
