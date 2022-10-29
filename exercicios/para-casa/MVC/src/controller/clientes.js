a1e09074537fa3b96597b5902b75f48f11825043
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-10-29 01:12:08 -0300

    create handler 'create client'

--- exercicios/para-casa/MVC/src/controller/clientes.js
+++ exercicios/para-casa/MVC/src/controller/clientes.js
@@ -1,4 +1,4 @@
-const create = (req, res) => {
+/*const create = (req, res) => {
   return res.send("ola mundo")
 }
 const show = (req, res) => {}
@@ -12,4 +12,45 @@ module.exports = {
   index,
   update,
   destroy
+}*/
+
+const listaClientesBanco = require('../model/contas-clientes.json')
+const { v4: uuidv4 } = require('uuid')
+const moment = require('moment')
+
+const criaCliente = (req, res) => {
+  const {
+    nome_cliente,
+    cpf_cliente,
+    data_nascimento,
+    conta: { tipo }
+  } = req.body
+
+  const clienteUnico = listaClientesBanco.find(
+    cliente => cliente.cpf_cliente == cpf_cliente
+  )
+
+  if (!clienteUnico) {
+    const novoCliente = {
+      id: uuidv4(),
+      nome_cliente: nome_cliente,
+      cpf_cliente: cpf_cliente,
+      data_nascimento: data_nascimento,
+      conta: {
+        numero: Math.random(),
+        tipo: tipo,
+        saldo: 0,
+        data_criacao: moment().format('l')
+      }
+    }
+    listaClientesBanco.push(novoCliente)
+    return res.status(201).json(novoCliente)
+  }
+  return res.status(404).json({
+    mensagem: 'Cliente já existe'
+  })
+}
+
+module.exports = {
+  criaCliente
 }
